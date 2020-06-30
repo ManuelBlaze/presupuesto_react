37c0c547c463f6f532f56f1b86395ff5bf2fd25f
ManuelBlaze <manuel.escobar1217@gmail.com>
2020-06-29 22:22:08 -0500

    Listar Gastos

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {  useState } from 'react';
 import Pregunta from "./components/Pregunta";
 import Formulario from "./components/Formulario";
+import Listado from "./components/Listado";
 
 function App() {
 
@@ -8,40 +9,47 @@ function App() {
   const [presupuesto, setPresupuesto] = useState(0);
   const [restante, setRestante] = useState(0);
   const [mostrarpregunta, actualizarPregunta] = useState(true);
+  const [gastos, setGastos] = useState([]);
+
+  //Cuando agreguemos un nuevo gasto
+  const setNuevoGasto = gasto => {
+    setGastos([
+      ...gastos,
+      gasto
+    ]);
+  }
 
 
   return (
-    <div className="container">
-      <header>
-        <h1>Gasto Semanal</h1>
+		<div className="container">
+			<header>
+				<h1>Gasto Semanal</h1>
 
-        <div className="contenido-principal contenido">
-          
-          {mostrarpregunta ? 
-            ( 
-              <Pregunta
-                setPresupuesto={setPresupuesto}
-                setRestante={setRestante}
-                actualizarPregunta={actualizarPregunta}
-              />
-            ) : (
-              <div className="row">
-                <div className="one-half column">
-                  <Formulario />
-                </div>
-                <div className="one-half column">
-                  2
-                </div>
+				<div className="contenido-principal contenido">
+					{mostrarpregunta ? (
+						<Pregunta
+							setPresupuesto={setPresupuesto}
+							setRestante={setRestante}
+							actualizarPregunta={actualizarPregunta}
+						/>
+					) : (
+						<div className="row">
+							<div className="one-half column">
+                <Formulario 
+                  setNuevoGasto={setNuevoGasto} 
+                />
+							</div>
+							<div className="one-half column">
+                <Listado 
+                  gastos = {gastos}
+                />
               </div>
-            ) 
-          }
-            
-        </div>
-      </header>
-
-    </div>
-    
-  );
+						</div>
+					)}
+				</div>
+			</header>
+		</div>
+	);
 }
 
 export default App;
--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -2,74 +2,74 @@ import React, { useState } from 'react';
 import Error from "./Error";
 import shortid from "shortid";
 
-const Formulario = () => {
+const Formulario = ({ setNuevoGasto }) => {
+	const [nombre, setNombre] = useState("");
+	const [cantidad, setCantidad] = useState('');
+	const [error, setError] = useState(false);
 
-    const [nombre, setNombre] = useState('');
-    const [cantidad, setCantidad] = useState(0);
-    const [error, setError] = useState(false);
+	//Cuando el usuario agrega un gasto
+	const setGasto = (e) => {
+		e.preventDefault();
 
-    //Cuando el usuario agrega un gasto
-    const setGasto = (e) => {
-        e.preventDefault();
-        
-        //Validar
-        if (cantidad < 1 || isNaN(cantidad) || nombre.trim() === '') {
-            setError(true);
-            return;
-        }
-        setError(false);
+		//Validar
+		if (cantidad < 1 || isNaN(cantidad) || nombre.trim() === "") {
+			setError(true);
+			return;
+		}
+		setError(false);
 
-        //Construir el gasto
-        const gasto = {
+		//Construir el gasto
+		const gasto = {
 			nombre,
 			cantidad,
-			id: shortid.generate()
+			id: shortid.generate(),
 		};
-        console.log(gasto);
 
-        //Pasar al componente principal
+		//Pasar al componente principal
+        setNuevoGasto(gasto);
 
         //Resetear Form
-    }
+        setNombre('');
+        setCantidad('');
+	};
 
-    return (
-        <form
-            onSubmit={setGasto}
-        >
-            <h2>Agrega tus gastos aquí</h2>
+	return (
+		<form onSubmit={setGasto}>
+			<h2>Agrega tus gastos aquí</h2>
 
-            {error ? <Error mensaje="Ambos campos son obligatorios o Presupuesto Incorrecto" /> : null}
+			{error ? (
+				<Error mensaje="Ambos campos son obligatorios o Presupuesto Incorrecto" />
+			) : null}
 
-            <div className="campo">
-                <label> Nombre Gasto</label>     
-                <input 
-                    type="text" 
-                    placeholder="Ej. Transporte" 
-                    className="u-full-width"
-                    value={nombre}
-                    onChange={e => setNombre(e.target.value)}
-                />
-            </div>
+			<div className="campo">
+				<label> Nombre Gasto</label>
+				<input
+					type="text"
+					placeholder="Ej. Transporte"
+					className="u-full-width"
+					value={nombre}
+					onChange={(e) => setNombre(e.target.value)}
+				/>
+			</div>
 
-            <div className="campo">
-                <label> Cantidad Gasto</label>     
-                <input 
-                    type="number" 
-                    placeholder="Ej. 3000" 
-                    className="u-full-width"
-                    value={cantidad}
-                    onChange={e => setCantidad(parseInt(e.target.value, 10))}
-                />
-            </div>
+			<div className="campo">
+				<label> Cantidad Gasto</label>
+				<input
+					type="number"
+					placeholder="Ej. 3000"
+					className="u-full-width"
+					value={cantidad}
+					onChange={(e) => setCantidad(parseInt(e.target.value, 10))}
+				/>
+			</div>
 
-            <input 
-                type="submit" 
-                value="Agregar Gasto" 
-                className="button-primary u-full-width"
-            />
-
-        </form>
-    );
+			<input
+				type="submit"
+				value="Agregar Gasto"
+				className="button-primary u-full-width"
+			/>
+		</form>
+	);
 };
 
 export default Formulario;
